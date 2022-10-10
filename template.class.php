9a1ff05d3bf881e2b9c506b29b3129391a3d5075
svetlin.ivanov <svetlin.ivanov@digital-solutions.vero.bg>
2022-10-10 12:19:34 +0300

    add tutorial contents

---
+++ template.class.php
@@ -0,0 +1,41 @@
+<?php
+
+class Template {
+    var $assignedValues = array();
+    var $tpl;
+
+    function __construct($_path = '')
+    {
+        if(!empty($_path)) {
+            if(file_exists($_path))
+            {
+                $this->tpl = file_get_contents($_path);
+            }
+            else {
+                echo "<b>Template Error:</b> File Inclusion Error.";
+            }
+        }
+        echo $this->tpl;
+    }
+
+    function assign($_searchString, $_replaceString)
+    {
+    if(!empty($_searchString))
+        {           
+            $this->assignedValues[strtoupper($_searchString)] = $_replaceString;
+        }  
+    }
+
+    function show()
+    {
+            if(count($this->assignedValues) > 0)
+        {
+            foreach ($this->assignedValues as $key => $value)
+                {
+                    $this->tpl = str_replace('{'.$key.'}', $value, $this->tpl);
+                }
+        }
+        echo $this->tpl;
+    }
+} 
+?> 
